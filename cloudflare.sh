4fe1b9791e2087b8e0c4982a2e612402bc36b6f2
Viljami Vastamäki <viljami.vastamaki@nordcloud.com>
2020-09-12 18:11:25 +0300

    Add dynamic CF script

---
+++ cloudflare.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Cloudflare zone
+zone=
+# A record which will be updated
+dnsrecord=
+
+# Credentials
+cloudflare_auth_email=
+cloudflare_auth_key=
+
+# Get the current external IP address
+ip=$(curl -s -X GET https://checkip.amazonaws.com)
+
+echo "Current IP is $ip"
+
+if host $dnsrecord 1.1.1.1 | grep "has address" | grep "$ip"; then
+  echo "$dnsrecord is currently set to $ip; no changes needed"
+  exit
+fi
+
+# get the zone id for the requested zone
+zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
+  -H "Authorization: Bearer $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+echo "Zoneid for $zone is $zoneid"
+
+# get the dns record id
+dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
+  -H "Authorization: Bearer $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+echo "DNSrecordid for $dnsrecord is $dnsrecordid"
+
+# update the record
+curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
+  -H "Authorization: Bearer $cloudflare_auth_key" \
+  -H "Content-Type: application/json" \
+  --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$ip\",\"ttl\":1,\"proxied\":true}" | jq
